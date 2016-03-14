6f6243cc33fa06b4cc719e8bd2a3f132aa98e631
drhemanth <phemanth88@gmail.com>
2016-03-14 15:08:59 +0000

    first commit

---
+++ src/RecursionTest.java
@@ -0,0 +1,25 @@
+/**
+ * Created by hemanth on 14/03/2016.
+ */
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+public class RecursionTest {
+    @Test
+    public void factorial0(){
+        Factorial f= new Factorial ();
+       int result = f.Calculate(1);
+        assertEquals(1,result);
+    }
+    @Test
+    public void factorial1(){
+        Factorial f= new Factorial ();
+        int result = f.Calculate(0);
+        assertEquals(1,result);
+    }
+    @Test
+    public void factorial2(){
+        Factorial f= new Factorial ();
+        int result = f.Calculateif(4);
+        assertEquals(2,result);
+    }
+}
